a6f0f104521884d7556d691c508d462287285e3b
Adi Mathur <65034167+IDoEverything@users.noreply.github.com>
2022-08-25 21:21:46 +0530

    custom benchmarks

--- README.md
+++ README.md
@@ -4,16 +4,18 @@ Drive benchmarking and partition listing tool, written in blazing-fast, ultra-me
 
 ## Installing
 
-Go to the [releases](https://github.com/IDoEverything/DriveReader/releases) page and download the appropriate executable for your distribution
+Go to the [releases](https://github.com/IDoEverything/DriveReader/releases) page and download the appropriate executable for your distribution from the latest release.
 
-Linux (x86):
+#### Linux (x86_64):
 
 ```
 > chmod +x executable_name
 > ./executable_name <args>
 ```
 
-Other platforms:
+Tested on Linux Mint 20.3
+
+#### Other platforms:
 
 * Install [cargo](https://doc.rust-lang.org/cargo/)
 * Clone this repository
@@ -24,10 +26,11 @@ Other platforms:
 
 ## Usage
 
-All commands must be executed as superuser, using `sudo` on unix-like systems or in a command prompt with elevated access on windows.
+All commands must be executed as superuser. For example, run with `sudo` on Unix-like systems or in a command prompt with elevated access on Windows.
 
 ### Partition listing
 
+Usage template:
 ```
 <executable> l <disk/path>
 ```
@@ -38,14 +41,48 @@ Example:
 ./DiskReader l /dev/sdb
 ```
 
+This lists all healthy and unhealthy partitions on the specified disk (limited to GPT formatted drives)
+
 ### Disk benchmarking
 
+Usage template:
 ```
-<executable> b <directory/path>
+<executable> b <directory/path> [custom benchmarks]
 ```
 
-Example:
+Simple example:
 
 ```
-./DiskReader b /home/
+./DiskReader b /home/<username>
 ```
+
+Without custom benchmarks specified, two are run by default:
+
+```
+8 packets of 1025mb each
+265 packets of 8mb each
+```
+
+You can specify your own benchmarks by adding arguments in the following form:
+
+```
+<executable b <directory/path> <b1 data amount (mb)> <b1 packet count> <b2 data amount (mb)> <b2 packet count> ...
+```
+
+For example:
+
+```
+./DriveReader b /home/<username> 32 4 8 10
+```
+
+This runs two benchmarks, one with 32mb written in 4 packets, and one with 8bm written in 10 packets.
+
+## Code explanation
+
+The basic functionality is documented in the code itself through comments.
+
+The list command uses the [gptman](https://docs.rs/crate/gptman/latest) library to obtain information about drives.
+
+The benchmarking command creates a file in the specified directory and writes data to it in the specified form while timing it.
+
+This all makes use of the Rust borrow checking system, as well as the excellent error handling features. This ensures that no memory leaks will take place, and most possible runtime errors will be properly caught and handled. The code compiles directly to machine language for optimal performance.
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::env;
 use std::fs::{File, remove_file};
 use std::io::{stdout, Write};
 use std::ptr::write;
+use std::str::FromStr;
 use std::time::Instant;
 
 fn main() {
@@ -14,6 +15,8 @@ fn main() {
     }
 
     let command = &args[1];
+
+    //partition list command
     if (command == "l") {
         if (args.len() < 3) {
             println!("Missing required argument: disk path");
@@ -22,7 +25,8 @@ fn main() {
 
         let drive_path = &args[2];
 
-        let mut f = match std::fs::File::open(drive_path) {
+        //load drive as file
+        let mut f = match File::open(drive_path) {
             Ok(thing) => thing,
             Err(_) => {
                 println!("could not open disk at path {drive_path} (could be missing root privileges)");
@@ -30,6 +34,7 @@ fn main() {
             }
         };
 
+        //get data from library
         let gpt = match gptman::GPT::find_from(&mut f) {
             Ok(thing) => thing,
             Err(_) => {
@@ -41,6 +46,7 @@ fn main() {
         println!("Disk {}", drive_path);
 
         for (i, p) in gpt.iter() {
+            //write size if healthy partition
             if p.is_used() {
                 let mut size: f64 = (p.size().unwrap() * gpt.sector_size) as f64;
                 let mut unit: &str = "bytes";
@@ -59,6 +65,7 @@ fn main() {
 
                 println!("Partition #{}: size = {} {}", i, size.trunc(), unit);
             }
+            //detect unhealthy partitions
             else {
                 let size = p.size().unwrap();
                 if (size != 1) {
@@ -67,6 +74,8 @@ fn main() {
             }
         }
     }
+
+    //benchmarking command
     else if (command == "b") {
         if (args.len() < 3) {
             println!("Missing required argument: directory path");
@@ -82,15 +91,37 @@ fn main() {
 
         println!("Running benchmarks");
 
-        write_repeated(&file_path, 1024, 8);
-        write_repeated(&file_path, 8, 256);
+        //check for custom benchmark values
+        if (args.len() > 3) {
+            if (args.len() % 2 == 0) {
+                println!("Invalid arguments provided: expected even number of additional arguments.");
+                return;
+            }
+
+            let num_args = args.len() - 3;
+            let i = 0;
+
+            //run custom benchmarks
+            while i < num_args / 2 {
+                let data_mb = &args[3 + i * 2].parse::<u128>().unwrap();
+                let packet_count = &args[3 + i * 2 + 1].parse::<u128>().unwrap();
+                write_repeated(&file_path, *data_mb, *packet_count);
+            }
+        }
+        else {
+            //default benchmark values
+            write_repeated(&file_path, 1024, 8);
+            write_repeated(&file_path, 8, 256);
+        }
     }
     else {
         println!("No command found matching {}", command);
     }
 }
 
+//function to handle benchmarking with specific values
 fn write_repeated(file_path: &String, data_mb: u128, packets: u128) {
+    //create file for benchmarking
     let mut f = match File::create(file_path) {
         Ok(thing) => thing,
         Err(_) => {
@@ -101,6 +132,7 @@ fn write_repeated(file_path: &String, data_mb: u128, packets: u128) {
 
     println!("Writing {}mb {} times", data_mb, packets);
 
+    //start overall timer
     let start = Instant::now();
 
     let mut min = 0;
@@ -108,9 +140,15 @@ fn write_repeated(file_path: &String, data_mb: u128, packets: u128) {
 
     let mut i = 1;
     while i <= packets {
+        //run individual timer
         let mut t = Instant::now();
+
+        //write correct amount of data
         f.write(&vec![u8::MAX; data_mb as usize * 1024 * 1024]).expect("Error writing to file");
+        //save
         f.sync_data().expect("Error saving to file");
+
+        //collect individual time and check against highest and lowest values
         let elapsed = t.elapsed().as_millis();
         if (min == 0 || elapsed < min) {
             min = elapsed;
@@ -119,21 +157,27 @@ fn write_repeated(file_path: &String, data_mb: u128, packets: u128) {
             max = elapsed;
         }
 
+        //progress bar
         print!("__ ");
         if (i % 20 == 0) {
             println!();
         }
+        //flush output stream to make sure progress bar prints
         stdout().flush().expect("Failed to flush output");
         i += 1;
     }
+    //delete file after running benchmarks
     remove_file(file_path).expect("Failed to delete file");
     let total_time = start.elapsed().as_millis();
 
+    //format and print benchmark statistics
     println!();
+
     println!("Total time: {} ms | {} s", total_time, total_time / 100);
     println!("Min time: {} ms | {} s", min, min / 100);
     println!("Max time: {} ms | {} s", max, max / 100);
-    println!("Average time: {} ms | {} s", total_time / packets, (total_time / 100) / packets);
-
+    println!("Average time per packet: {} ms | {} s", total_time / packets, (total_time / 100) / packets);
     println!("Average speed: {} MB/s", (packets * data_mb) / (total_time / 100));
+
+    println!();
 }
